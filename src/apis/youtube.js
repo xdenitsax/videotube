cd41d4c84327ddb142de89e4b0f99202c98eea61
Denitsa Georgieva <denittsaa@yahoo.com>
2021-06-07 12:09:24 -0600

    Hide.

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyAANFZaTn_yvyC-saA5Cr9P5SrjMv9UOa0'
+// const KEY = 'AIzaSyAANFZaTn_yvyC-saA5Cr9P5SrjMv9UOa0'
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
